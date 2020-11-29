3933cbae430b2553e10d91257b649b1b9d809ec1
kevinnivekkevin <63487456+kevinnivekkevin@users.noreply.github.com>
2020-11-29 14:55:36 +0800

    Update process_reg.php

--- MDP/Participant/process_reg.php
+++ MDP/Participant/process_reg.php
@@ -5,35 +5,48 @@ $page_title = 'Register';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include ('mysqli_connect.php');
     $errors = array(); // Initialize an error array.   
-    $fn = mysqli_real_escape_string($dbc, trim($_POST['first_name']));      
+//    $fn = mysqli_real_escape_string($dbc, trim($_POST['first_name']));      
+    $fn = htmlspecialchars($_POST['first_name']);
     // Check for a last name:
     if (empty($_POST['last_name'])) {
         $errors[] = 'last name required.';
     } else {
-        $ln = mysqli_real_escape_string($dbc, trim($_POST['last_name']));
+//        $ln = mysqli_real_escape_string($dbc, trim($_POST['last_name']));
+        $ln = htmlspecialchars($_POST['last_name']);
     }
     
      if (empty($_POST['dob_year']) || empty($_POST['dob_month']) || empty($_POST['dob_day'])) {
         $errors[] = 'You forgot to enter your date of birth.';
     } else {
-        $y = mysqli_real_escape_string($dbc, trim($_POST['dob_year']));
-        $m = mysqli_real_escape_string($dbc, trim($_POST['dob_month']));
-        $d = mysqli_real_escape_string($dbc, trim($_POST['dob_day']));
+//        $y = mysqli_real_escape_string($dbc, trim($_POST['dob_year']));
+//        $m = mysqli_real_escape_string($dbc, trim($_POST['dob_month']));
+//        $d = mysqli_real_escape_string($dbc, trim($_POST['dob_day']));
+        $y = htmlspecialchars($_POST['dob_year']);
+        $m = htmlspecialchars($_POST['dob_month']);
+        $d = htmlspecialchars($_POST['dob_day']);
     }
     
     
     if (empty($_POST['gender'])) {
         $errors[] = 'You forgot to enter your gender.';
     } else {
-        $g = mysqli_real_escape_string($dbc, trim($_POST['gender']));
+//        $g = mysqli_real_escape_string($dbc, trim($_POST['gender']));
+        $g = htmlspecialchars($_POST['gender']);
     }
-    
+
+    if (empty($_POST['blood_type'])) {
+        $errors[] = 'You forgot to enter your blood type.';
+    } else {
+//        $g = mysqli_real_escape_string($dbc, trim($_POST['gender']));
+        $bt = htmlspecialchars($_POST['blood_type']);
+    }
     
     // Check for an email address:
     if ($_POST['email']=="E-mail") {
         $errors[] = 'email address required';
     } else {
-        $e = mysqli_real_escape_string($dbc, trim($_POST['email']));
+//        $e = mysqli_real_escape_string($dbc, trim($_POST['email']));
+        $e = htmlspecialchars($_POST['email']);
     }
  
     // Check for a password and match against the confirmed password:
@@ -41,18 +54,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($_POST['pass'] != $_POST['pass1']) {
             $errors[] = 'password do not match';
         } else {
-            $p = mysqli_real_escape_string($dbc, trim($_POST['pass1']));
+//            $p = mysqli_real_escape_string($dbc, trim($_POST['pass1']));
+            $p = htmlspecialchars($_POST['pass1']);
+            $p = password_hash($p, PASSWORD_DEFAULT);
         }
     } else {
         $errors[] = 'password required';
     }
     
-    $un = mysqli_real_escape_string($dbc, trim($_POST['username']));
+//    $un = mysqli_real_escape_string($dbc, trim($_POST['username']));
+    $un = htmlspecialchars($_POST['username']);
      
     if (empty($errors)) { // If everything's OK.     
         // Register the user in the database...         
         // Make the query:
-        $q = "INSERT INTO vending.acc (first_name, last_name, username, email, dob_day, dob_month, dob_year, gender, pass, registration_date) VALUES ('$fn', '$ln', '$un', '$e', '$d', '$m', '$y', '$g', SHA1('$p'), NOW() )";      
+        $q = "INSERT INTO vending.acc (first_name, last_name, username, email, dob_day, dob_month, dob_year, gender, pass, bloodtype,registration_date) VALUES ('$fn', '$ln', '$un', '$e', '$d', '$m', '$y', '$g', '$p','$bt',NOW() )";      
         $r = mysqli_query ($dbc, $q); // Run the query.
         if ($r) { // If it ran OK.         
             // Print a message:
@@ -76,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
              foreach ($errors as $msg) { // Print each error.
             echo " - $msg<br />\n";
         }
-            echo "<a href='register.php' class='btn btn-warning' type>Return to Sign Up</a>";
+            echo "<a href='Reg_Acc.php' class='btn btn-warning' type>Return to Sign Up</a>";
             //echo '<button type="button" class="btn btn-danger"><a href="register.php">Return to Sign Up</a></button>' ;
         }          
     mysqli_close($dbc); // Close the database connection.
