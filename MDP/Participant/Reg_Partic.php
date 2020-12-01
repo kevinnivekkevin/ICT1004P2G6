9f8150db2a957e238cca5c15134384e60d358f9e
Ayesir2104 <72613257+Ayesir2104@users.noreply.github.com>
2020-12-01 21:00:09 +0800

    Update Reg_Partic.php

--- MDP/Participant/Reg_Partic.php
+++ MDP/Participant/Reg_Partic.php
@@ -68,14 +68,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-                    <link rel="stylesheet" href="css/style2.css">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                    <link rel="stylesheet" href="css/style3.css">
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--jQuery-->
+        <script defer    
+        src="https://code.jquery.com/jquery-3.4.1.min.js"    
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="    
+        crossorigin="anonymous">
+        </script>
+        
+        <!--Bootstrap JS-->
+        <script defer   
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"    
+        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"    
+        crossorigin="anonymous">
+        </script>
+        <!-- Custom JS-->
+                <script defer src="js/main.js"></script>
                 </head>
                 <body>
             <?php include "navbarParticipant.php"?>
-                    <div class="header" >
-                        <h1>Pack Reservation</h1>
-                    </div> 
+                    
                     <div class="container">
+                        <h1>Pack Reservation</h1>
             <?php
             echo '<h1>Thank you for registering for ' . $Event_Name . '!</h1>';
             echo '<h2>Click <a href="Pack_Registration.php">here</a> to reserve your shirt and water bottle for collection!</h2>';
