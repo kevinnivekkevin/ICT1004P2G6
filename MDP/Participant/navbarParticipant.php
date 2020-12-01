cff1544dc0d4f9554afdf24d092597032b22ab39
Nigouz <75168577+Nigouz@users.noreply.github.com>
2020-12-01 12:13:04 +0800

    Update navbarParticipant.php

--- MDP/Participant/navbarParticipant.php
+++ MDP/Participant/navbarParticipant.php
@@ -6,7 +6,7 @@ $username = $_SESSION['username'];
 <div class="navbar">
   <a style="width: 19%;" href="index.php">Home</a>
   <a style="width: 19%;" href="home.php">Account</a>
-  <a class="active" style="width: 22%;" href="Shirtsize.php">Shirt Size</a>
+  <a style="width: 22%;" href="Shirtsize.php">Shirt Size</a>
   <a style="width: 24%;" href="Pack_Registration.php">Reservation</a>
   <a style="width: 24%;" href="StockStatus.php">Stock status</a>
   <a style="width: 35%;" href="Reg_Partic.php">Event register</a>
