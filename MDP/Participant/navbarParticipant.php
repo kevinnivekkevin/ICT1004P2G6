04cbd793a2ef11ae71b3575b8a2cdbf73fa9583f
kevinnivekkevin <63487456+kevinnivekkevin@users.noreply.github.com>
2020-12-01 14:25:21 +0800

    Update navbarParticipant.php

--- MDP/Participant/navbarParticipant.php
+++ MDP/Participant/navbarParticipant.php
@@ -10,5 +10,6 @@ $username = $_SESSION['username'];
   <a style="width: 24%;" href="Pack_Registration.php">Reservation</a>
   <a style="width: 24%;" href="StockStatus.php">Stock status</a>
   <a style="width: 35%;" href="Reg_Partic.php">Event register</a>
+  <a style="width: 35%;" href="myReservations.php">My Reservations</a>
   <?php echo "<p>User: "; echo $_SESSION['username']; echo "</p>";?>
 </div>
