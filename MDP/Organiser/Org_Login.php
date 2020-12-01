5e602f3f7125773787aa8a6bf0b74c410c081ced
Ayesir2104 <72613257+Ayesir2104@users.noreply.github.com>
2020-12-01 22:01:14 +0800

    Update Org_Login.php

--- MDP/Organiser/Org_Login.php
+++ MDP/Organiser/Org_Login.php
@@ -51,7 +51,28 @@ if(password_verify($password, $hash_pass)){
 
   <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 
-      <link rel="stylesheet" href="css/style4.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style3.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet">
+        <!--jQuery-->
+        <script defer    
+        src="https://code.jquery.com/jquery-3.4.1.min.js"    
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="    
+        crossorigin="anonymous">
+        </script>
+        
+        <!--Bootstrap JS-->
+        <script defer   
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"    
+        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"    
+        crossorigin="anonymous">
+        </script>
+        <!-- Custom JS-->
+<script defer src="js/main.js"></script>
 </head>
 <body>
       <script>
@@ -67,10 +88,12 @@ function onFocus(el) {
 }
 </script>
     
-    <div class="header" >
-	<h1>Admin Login</h1>
-       </div> 
+   <?php
+        include 'nav.inc.php';
+    ?>
+     
     <div class="container">
+        <h1>Admin Login</h1>
 	<div class="row" >
 	<h2 style="color: #f00;"> <?php echo $error; ?></h2>
        </div>
